c83da3958e5e8e1dfbe4a21922467490f59dbe00
Jeremiah Rivera <riverajeremiah10@gmail.com>
2026-04-15 20:11:05 -0400

    3rd fix for deployment

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,11 @@ const nextConfig: NextConfig = {
   rewrites: async () => {
     return [
       {
-        // When you call /api/something, it routes to the Python server in development
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*' // Your local Python server
-            : '/api/', // Vercel's production path
+            ? 'http://127.0.0.1:8000/api/:path*' // Local FastAPI
+            : '/api/index', // Vercel Production FastAPI
       },
     ]
   },
